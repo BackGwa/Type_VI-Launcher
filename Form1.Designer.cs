fac7e1bce046dd060806a92da12f7aa31a9b28a2
BackGwa <64456214+BackGwa@users.noreply.github.com>
2022-06-08 10:34:17 +0900

    Launcher

---
+++ Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 8F);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(121)))), ((int)(((byte)(235)))));
+            this.label1.Location = new System.Drawing.Point(156, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "{Stat}";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(76)))), ((int)(((byte)(121)))), ((int)(((byte)(235)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(437, 669);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VOCALINE";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Text;
+using System.Net;
+using System.ComponentModel;
+
+namespace Launcher
+{
+        public partial class Form1 : Form
+    {
+
+        WebClient webClient1;
+
+
+        //폼 투명화
+        public Form1()
+        {
+            InitializeComponent();
+            this.TransparencyKey = Color.FromArgb(52, 105, 173);
+            this.BackColor = Color.FromArgb(52, 105, 173);
+            this.FormBorderStyle = FormBorderStyle.None;
+        }
+
+
+
+
+
+
+        //딜레이 함수 정의
+            public void Delay(int ms)
+        {
+            DateTime dateTimeNow = DateTime.Now;
+            TimeSpan duration = new TimeSpan(0, 0, 0, 0, ms);
+            DateTime dateTimeAdd = dateTimeNow.Add(duration);
+            while (dateTimeAdd >= dateTimeNow)
+            {
+                System.Windows.Forms.Application.DoEvents();
+                dateTimeNow = DateTime.Now;
+            }
+            return;
+        }
+
+
+
+
+
+
+        //폰트 정의
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+
+            PrivateFontCollection privateFonts = new PrivateFontCollection();
+            privateFonts.AddFontFile("FONT.TTF");
+
+            Font font = new Font(privateFonts.Families[0], 8f);
+
+            label1.Font = font;
+        }
+
+
+
+
+
+
+        //체킹 메세지 정의
+        void CheckingText()
+        {
+
+            label1.Text = "Checking update package.";
+            Delay(250);
+            label1.Text = "Checking update package..";
+            Delay(250);
+            label1.Text = "Checking update package...";
+            Delay(250);
+
+        }
+
+
+
+
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+
+
+
+            //체킹 메세지 메세지 출력
+            label1.Text = "Launcher Version : Stable1.0";
+            Delay(2000);
+            CheckingText();
+
+            //설치 파일 확인
+            string _Locate = @"C:\VOCALINE\VOCALINE\Content\Paks\VOCALINE-WindowsNoEditor.pak";
+            System.IO.FileInfo filepath = new System.IO.FileInfo(_Locate);
+            if (filepath.Exists)
+            {
+
+                launch();
+
+            }
+
+            else
+            {
+
+                string downloadurl = "http://vlapi.kro.kr/";
+                WebClient webClient = new WebClient();
+                WebClient wc = new WebClient();
+                label1.Text = "Download Package Files";
+                wc.DownloadFile(downloadurl, @"C:\VOCALINE\package.zip");
+
+
+            }
+
+
+            void launch()
+            {
+
+
+
+                //패키지 경로 지정
+                string updatefile = @"C:\VOCALINE\VOCALINE\Content\Paks\clientfile.qud";
+                string move = @"C:\VOCALINE\VOCALINE\Content\Paks\VOCALINE-WindowsNoEditor.pak";
+                string _Filestr = @"C:\VOCALINE\VOCALINE\Content\Paks\clientfile.qud";
+                string filePath = @"C:\VOCALINE\VOCALINE\Binaries\Win64\VOCALINE-Win64-Shipping.exe";
+                System.IO.FileInfo fi = new System.IO.FileInfo(_Filestr);
+
+
+
+                //업데이트 패키지가 있는가
+                if (fi.Exists)
+                {
+                    label1.Text = "Applying a new update package...";
+                    System.IO.File.Delete(move);
+                    System.IO.File.Move(updatefile, move);
+                    Delay(750);
+                    label1.Text = "Starting the process...";
+                    System.Diagnostics.Process.Start(filePath);
+                    Close();
+                }
+
+
+                else
+                {
+                    label1.Text = "update package not found.";
+                    Delay(750);
+                    label1.Text = "Starting the process...";
+                    System.Diagnostics.Process.Start(filePath);
+                    Close();
+                }
+
+            }
+
+
+
+
+        }
+    }
+}
